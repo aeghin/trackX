14f4dd153a1ba1ea682c562bef42fcc650d89bb6
Edward Castaneda <aeghin7@gmail.com>
2023-07-17 17:50:01 -0500

    fixing log in with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import Login from './components/Login/Login.jsx';
-
-
-
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+
+  const authenticated = Boolean(useSelector((state) => state.token));
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={authenticated ? <Dashboard /> : <Home />} />
           <Route path="/auth/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard only if logged in</div>
+  )
+}
+
+export default Dashboard
