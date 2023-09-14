6b53754ba370db8b80957699833d2dd03865d18a
Edward Castaneda <aeghin7@gmail.com>
2023-09-13 21:37:53 -0500

    redux state is properly working

--- client/src/scenes/Dashboard/Dashboard.jsx
+++ client/src/scenes/Dashboard/Dashboard.jsx
@@ -1,10 +1,42 @@
-import Navbar from "scenes/Navbar/Navbar"
+import Navbar from "scenes/Navbar/Navbar";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setProjects } from "state";
+
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const projects = useSelector((state) => state.projects) || [];
+  const token = useSelector((state) => state.token);
+
+  const getProjects = async () => {
+    const response = await fetch("http://localhost:3001/projects/projects",
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+    const data = await response.json();
+ 
+    dispatch(setProjects(data))
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <Navbar />
-    // <div>Dashboard only if logged in</div>
+    <>
+      <Navbar />
+      <div className="flex justify-center">Dashboard only if logged in</div>
+      {projects.map(({ _id, title }) => (
+        <ul>
+          <li key={_id}>{title}</li>
+        </ul>
+      )
+      )}
+    </>
   )
-}
+};
 
 export default Dashboard
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,4 +2,10 @@
 export const content = [
   './src/**/*.{js,jsx,tsx}',
 ];
+
+// export const theme = {
+//   colors: {
+//     'slate': '#94a3b8',
+//   }
+// };
 export const plugins = [require("tailwindcss-animate")];
