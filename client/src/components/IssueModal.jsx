2baca68cfff4812f076b1fbca3e73d0a0f389a88
Edward Castaneda <aeghin7@gmail.com>
2023-09-29 22:51:31 -0500

    added issue modal with state

---
+++ client/src/components/IssueModal.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+export const IssueModal = ({ onClose }) => {
+
+    const user = useSelector(state => state.user);
+
+    const [issueData, setIssueData] = useState({
+        title: '',
+        description: '',
+        created_by: user._id,
+        status: '',
+    });
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div className="bg-white p-8 rounded-lg w-96 relative">
+                <button onClick={onClose} className="absolute top-4 right-4 hover:text-red-600">X</button> {/* This button is inside the modal */}
+                <h2 className="text-2xl mb-4">Add a New Issue</h2>
+                <input
+                    type="text"
+                    className="border p-2 w-full mb-4"
+                    placeholder="title"
+                    value={title}
+                    onChange={(e) => setIssueData(e.target.value)}
+                />
+                <input
+                    type="text"
+                    className="border p-2 w-full mb-4"
+                    placeholder="description"
+                    value={description}
+                    onChange={(e) => setIssueData(e.target.value)}
+                />
+                <button
+                    // onClick={handleSubmit}
+                    className="bg-gray-500 text-white p-2 hover:bg-indigo-500 rounded"
+                >
+                    Add Issue
+                </button>
+            </div>
+        </div>
+    )
+};
--- client/src/components/Issues.jsx
+++ client/src/components/Issues.jsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setIssues } from "state";
 import { IssuesCard } from "./IssuesCard";
+import { IssueModal } from "./IssueModal";
+
+
 export const Issues = () => {
+
   const token = useSelector(state => state.token);
   const dispatch = useDispatch();
   const issue = useSelector(state => state.issues);
   const project = useSelector(state => state.projects);
   const { projectId } = useParams();
+  const [isModalOpen, setModalOpen] = useState(false);
+
+
   const getIssues = async () => {
     const response = await fetch(`http://localhost:3001/projects/${projectId}/issues`,
       {
@@ -29,17 +36,25 @@ export const Issues = () => {
   const projects = project.find(proj => proj._id === projectId);
 
   return (
-    <div className="min-h-screen flex">
-      <div className="w-full p-8 bg-gray-100">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl py-2 px-6 rounded-lg bg-gray-300 font-semibold">{projects.title}</h2>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {issue && issue[projectId] && issue[projectId].map((issueItem) => (
-            <IssuesCard key={issueItem._id} title={issueItem.title} />  
-          ))}
+    <>
+      <div className="min-h-screen flex">
+        <div className="w-full p-8 bg-gray-100">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl py-2 px-6 rounded-lg bg-gray-300 font-semibold">{projects.title}</h2>
+            <button
+              onClick={() => setModalOpen(true)}
+              className="bg-gray-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
+              + Add Issue
+            </button>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {issue && issue[projectId] && issue[projectId].map((issueItem) => (
+              <IssuesCard key={issueItem._id} title={issueItem.title} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      {isModalOpen && <IssueModal token={token} projectId={projectId} onClose={() => setModalOpen(false)} />}
+    </>
   )
 };
